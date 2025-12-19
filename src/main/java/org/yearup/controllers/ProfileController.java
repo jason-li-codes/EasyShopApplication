956fd360f0eb493a692ca11fccca774055c4e3ce
lijas <lijason991@gmail.com>
2025-12-18 21:43:41 -0500

    completed required Mappings for ProfileController

---
+++ src/main/java/org/yearup/controllers/ProfileController.java
@@ -0,0 +1,68 @@
+package org.yearup.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.yearup.data.ProfileDao;
+import org.yearup.data.UserDao;
+import org.yearup.models.Profile;
+import org.yearup.models.User;
+
+import java.security.Principal;
+
+@RestController
+@CrossOrigin
+@RequestMapping("profile")
+public class ProfileController {
+
+    private ProfileDao profileDao;
+    private UserDao userDao;
+
+    @Autowired
+    public ProfileController(ProfileDao profileDao, UserDao userDao) {
+        this.profileDao = profileDao;
+        this.userDao = userDao;
+    }
+
+    @GetMapping("")
+    @PreAuthorize("isAuthenticated()")
+    public Profile getProfileById(Principal principal) {
+
+        try {
+            // get the currently logged-in username
+            String userName = principal.getName();
+            // find database user by userId
+            User user = userDao.getUserByUserName(userName);
+            int userId = user.getId();
+            // use the profileDao to get the right profile
+            return profileDao.getProfileByUserId(userId);
+        } catch (ResponseStatusException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+        }
+    }
+
+    @PutMapping("")
+    @PreAuthorize(("isAuthenticated()"))
+    public Profile updateProfile(Principal principal, @RequestBody Profile profile) {
+
+        try {
+            // get the currently logged-in username
+            String userName = principal.getName();
+            // find database user by userId
+            User user = userDao.getUserByUserName(userName);
+            int userId = user.getId();
+
+            profileDao.update(userId, profile);
+            return profile;
+
+        } catch (ResponseStatusException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+        }
+    }
+}
