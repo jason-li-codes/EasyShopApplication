101b925d418dc9b7d369ca774a9739b5ddf07277
lijas <lijason991@gmail.com>
2025-12-18 21:44:25 -0500

    created OrderService to complete transactions for user's ShoppingCart

---
+++ src/main/java/org/yearup/services/OrderService.java
@@ -0,0 +1,94 @@
+package org.yearup.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+import org.yearup.data.OrderDao;
+import org.yearup.data.OrderLineItemDao;
+import org.yearup.data.ProfileDao;
+import org.yearup.data.ShoppingCartDao;
+import org.yearup.models.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Service
+public class OrderService {
+
+    private OrderDao orderDao;
+    private OrderLineItemDao orderLineItemDao;
+    private ShoppingCartDao shoppingCartDao;
+    private ProfileDao profileDao;
+
+    @Autowired
+    public OrderService(OrderDao orderDao, OrderLineItemDao orderLineItemDao,
+                        ShoppingCartDao shoppingCartDao, ProfileDao profileDao) {
+        this.orderDao = orderDao;
+        this.orderLineItemDao = orderLineItemDao;
+        this.shoppingCartDao = shoppingCartDao;
+        this.profileDao = profileDao;
+    }
+
+    public Order checkout(int userId) {
+
+        Order order = new Order();
+
+        createOrder(userId, order);
+        orderDao.create(order);
+
+        ShoppingCart cart = shoppingCartDao.getByUserId(userId);
+
+        if (cart == null || cart.getItems().isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Cart is empty.");
+        }
+
+        Map<Integer, ShoppingCartItem> itemList = cart.getItems();
+
+        for (Map.Entry<Integer, ShoppingCartItem> i : itemList.entrySet()) {
+
+            OrderLineItem orderLineItem = new OrderLineItem();
+
+            int productId = i.getKey();
+            Product product = i.getValue().getProduct();
+
+            orderLineItem.setOrderId(order.getOrderId());
+            orderLineItem.setProductId(productId);
+            orderLineItem.setSalesPrice(product.getPrice());
+            orderLineItem.setQuantity(i.getValue().getQuantity());
+            orderLineItem.setDiscount(i.getValue().getDiscountPercent());
+
+            orderLineItemDao.create(orderLineItem);
+        }
+
+        shoppingCartDao.deleteShoppingCart(userId);
+        return order;
+    }
+
+    @Transactional
+    private void createOrder(int userId, Order order) {
+
+        Profile profile = profileDao.getProfileByUserId(userId);
+
+        if (profile == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Profile not found.");
+        }
+
+        LocalDateTime dateTime = LocalDateTime.now();
+
+        String address = profile.getAddress();
+        String state = profile.getState();
+        String city = profile.getCity();
+        String zip = profile.getZip();
+
+        order.setUserId(userId);
+        order.setDate(dateTime);
+        order.setAddress(address);
+        order.setState(state);
+        order.setCity(city);
+        order.setZip(zip);
+        order.setShippingAmount(new BigDecimal("0"));
+    }
+}
